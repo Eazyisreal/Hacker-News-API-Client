438e2d84444636a80a16d9d891f1aef616ae2a6b
Eazyisreal <eazyisreal192@gmail.com>
2023-01-05 16:26:05 +0100

    Chore: Fetch data from api

--- news/items/views.py
+++ news/items/views.py
@@ -2,19 +2,30 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from .models import News
 from django.core.paginator import Paginator
+import requests
 
 class NewsListView(ListView):
     def get(self, request):
         type = request.GET.get('type')
         search = request.GET.get('search')
-        news_items = News.objects.all()
+        
+        # Fetch news items from the API
+        api_url = 'https://hacker-news.firebaseio.com/v0/topstories.json'
+        params = {}
         if type:
-            news_items = news_items.filter(item_type=type)
+            params['type'] = type
         if search:
-            news_items = news_items.filter(title__icontains=search)
-        news_items = news_items.order_by('-item_id')
-        paginator = Paginator(news_items, 10) # Show 10 items per page
+            params['search'] = search
+        response = requests.get(api_url, params=params)
+        news_items = response.json()
+        
+        # Filter and paginate the news items
+        paginator = Paginator(news_items, 5) # Show 5 items per page
         page = request.GET.get('page')
         news_items = paginator.get_page(page)
         context = {'news_items': news_items}
         return render(request, 'news_list.html', context)
+
+
+
+    
